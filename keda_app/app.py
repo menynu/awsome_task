01fb3077382a9dff699cbab76d18b044d404bff8
meny205@gmail.com <meny205@gmail.com>
2024-05-14 08:14:24 +0300

    adjusted some fixes

--- keda_app/app.py
+++ keda_app/app.py
@@ -1,17 +1,25 @@
 from flask import Flask, jsonify
 import docker
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
 client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+
+# Define the metric outside of the request handling logic
+container_gauge = metrics.info('app_containers', 'Number of running containers')
 
 @app.route('/')
 def home():
     return "Welcome to the Flask App!"
+
 @app.route('/containers')
 def list_containers():
     containers = client.containers.list()
-    names = [container.name for container in containers]
-    return jsonify(names)
+    container_count = len(containers)
+    container_gauge.set(container_count)  # Update the gauge with the current container count
+    return jsonify([container.name for container in containers])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
